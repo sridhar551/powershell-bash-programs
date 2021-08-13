695e068b7868bf0cd7c37db08304f20dfb04b441
Sridhar T <sridhar.tanda260@gmail.com>
2021-08-13 09:38:35 +0530

    Update update-kubeconfig.sh

--- update-kubeconfig.sh
+++ update-kubeconfig.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 echo "updating kubecontext of staging enviroment"
-aws eks --region REGION update-kubeconfig --name krysp-eks --role ROLE-ARN
+aws eks --region REGION update-kubeconfig --name EKS-CLUSTER-NAME --role ROLE-ARN
 
 echo "Creating velero environment"
 velero install --provider aws --plugins velero/velero-plugin-for-aws:v1.0.1 --bucket $VELERO_BUCKET --backup-location-config region=$AWS_REGION --snapshot-location-config region=$AWS_REGION --secret-file ./velero-credentials
