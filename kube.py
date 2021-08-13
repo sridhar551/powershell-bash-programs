4f9d3f10ade4d949249f55647dac90fe73ed4dde
Sridhar T <sridhar.tanda260@gmail.com>
2021-08-13 09:38:05 +0530

    Update kube.py

--- kube.py
+++ kube.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("aws eks --region us-east-1 update-kubeconfig --name krysp-eks --role role-arn")
+os.system("aws eks --region REGION update-kubeconfig --name EKS_CLUSTER_NAME --role ROLE-ARN")
 #os.system("velero install --provider aws --plugins velero/velero-plugin-for-aws:v1.0.1 --bucket $VELERO_BUCKET --backup-location-config region=$AWS_REGION --snapshot-location-config region=$AWS_REGION --#secret-file ./velero-credentials")
 os.system("velero restore create --from-backup nginx-controller-backup")
